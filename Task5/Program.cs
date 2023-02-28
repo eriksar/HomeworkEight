59c643ff50c1c1e1a9feb8a900abd8201b2244bc
Erik <erik.97@list.ru>
2023-02-28 19:36:25 +0400

    ДЗ

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -7,34 +7,34 @@
 10 09 08 07
 */
 
-void FillArraySpiral(int[,] array, int n)
+void FillArraySpiral(int[,] matrix, int n)
 {
     int i = 0, j = 0;
     int value = 1;
     for (int e = 0; e < n * n; e++)
     {
         int k = 0;
-        do { array[i, j++] = value++; } while (++k < n - 1);
-        for (k = 0; k < n - 1; k++) array[i++, j] = value++;
-        for (k = 0; k < n - 1; k++) array[i, j--] = value++;
-        for (k = 0; k < n - 1; k++) array[i--, j] = value++;
+        do { matrix[i, j++] = value++; } while (++k < n - 1);
+        for (k = 0; k < n - 1; k++) matrix[i++, j] = value++;
+        for (k = 0; k < n - 1; k++) matrix[i, j--] = value++;
+        for (k = 0; k < n - 1; k++) matrix[i--, j] = value++;
         ++i; ++j;
         n = n < 2 ? 0 : n - 2;
     }
 }
 
-void PrintArray(int[,] array)
+void PrintArray(int[,] matrix)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
+        for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            if (array[i, j] < 10)
+            if (matrix[i, j] < 10)
             {
-                Console.Write("0" + array[i, j]);
+                Console.Write("0" + matrix[i, j]);
                 Console.Write(" ");
             }
-            else Console.Write(array[i, j] + " ");
+            else Console.Write(matrix[i, j] + " ");
         }
         Console.WriteLine();
     }
